798be4dd5e617b26fb4a945fe657c2a932272d83
michele <gmvalfre@yahoo.it>
2023-02-08 16:48:21 +0100

    map generation doesn't plot anymore

--- MAIN.py
+++ MAIN.py
@@ -6,14 +6,53 @@ from procedural_city_generation.building_generation.main import main as building
 
 
 
-singleton = Singleton('roadmap')
-roadmap_main()
-singleton.kill()
+def generateCity():
+    singleton = Singleton('roadmap')
+    singleton.plot = False
+    roadmap_main()
+    singleton.kill()
 
-singleton = Singleton('polygons')
-polygons_main(None)
-singleton.kill()
+    singleton = Singleton('polygons')
+    singleton.plotbool = False
+    polygons_main(None)
+    singleton.kill()
 
-singleton = Singleton('building_generation')
-building_generation_main()
-singleton.kill()
+    singleton = Singleton('building_generation')
+    building_generation_main()
+    singleton.kill()
+
+
+def finalize():
+    import pickle
+    global path
+    import os
+    path=os.path.join(os.path.dirname(__file__),"procedural_city_generation")
+    ### IF ON WINDOWS, OPEN THIS FILE WITH BLENDER AND OVERWRITE       ###
+    ### FOLLOWING LINE WITH YOUR PATH AND UNCOMMENT THE FOLLOWING LINE ###
+    # path = "/home/jonathan/procedural_city_generation/procedural_city_generation/"
+    import json
+    global conf_values
+
+    with open(path+"/inputs/visualization.conf", 'r') as f:
+        conf_values=json.loads(f.read())
+    with open(path+"/temp/"+conf_values[u'input_name'][u'value']+"_heightmap.txt", 'r') as f:
+        filename=f.read()
+    with open(path+"/temp/"+filename, 'rb') as f:
+        points, triangles = pickle.loads(f.read())
+
+    with open(path+"/outputs/"+conf_values[u'input_name'][u'value']+".txt", 'rb') as f:
+        polygons=pickle.loads(f.read())
+    print(len(points), len(triangles), len(polygons))
+
+
+from direct.showbase.ShowBase import ShowBase
+
+class Generator(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        generateCity()
+        finalize()
+
+generator = Generator()
+generator.run()
